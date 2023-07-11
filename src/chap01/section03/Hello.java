2cfeaf07dc1a715888977d6299df08e10c922908
Gwanwoo-Park <49912860+Gwanwoo-Park@users.noreply.github.com>
2023-07-11 14:14:51 +0900

    Initial commit

---
+++ src/chap01/section03/Hello.java
@@ -0,0 +1,23 @@
+package chap01.section03;
+
+public class Hello {
+
+    // 접근제한자 , 타입 , 함수이름 (파라미터)
+
+    public void plus() {
+        int x = 3;
+    }
+
+    public static void main(String[] args) {
+        int age = 17;
+        int salary = 3920;
+
+        while(true) {
+
+        }
+
+        System.out.println(age);
+        System.out.println(salary + age);
+
+    }
+}
